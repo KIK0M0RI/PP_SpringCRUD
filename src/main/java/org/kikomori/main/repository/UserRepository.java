171332e5ac220abb815f69c16eaab3c4b13106cf
KIK0M0RI <kudashev.020@gmail.com>
2023-04-10 02:27:18 +0300

    adding repository and service

---
+++ src/main/java/org/kikomori/main/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.kikomori.main.repository;
+
+import org.kikomori.main.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
