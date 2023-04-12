d13bf4c320495f27bade4256a99238629b36d2a6
KIK0M0RI <kudashev.020@gmail.com>
2023-04-12 21:38:48 +0300

    Added an interface for the service layer

--- src/main/java/org/kikomori/main/service/UserService.java
+++ src/main/java/org/kikomori/main/service/UserService.java
@@ -1,47 +1,18 @@
 package org.kikomori.main.service;
 
 import org.kikomori.main.model.User;
-import org.kikomori.main.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-@Transactional(readOnly = true)
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
+    List<User> findAll();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    User findById(long id);
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
+    void save(User user);
 
-    public User findById(long id) {
-        Optional<User> foundUser = userRepository.findById(id);
-        return foundUser.orElse(null);
-    }
+    void update(long id, User user);
 
-    @Transactional
-    public void save(User user) {
-        userRepository.save(user);
-    }
-
-    @Transactional
-    public void update(long id, User user) {
-        user.setId(id);
-        userRepository.save(user);
-    }
-
-    @Transactional
-    public void deleteById(long id) {
-        userRepository.deleteById(id);
-    }
+    void deleteById(long id);
 }
---
+++ src/main/java/org/kikomori/main/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package org.kikomori.main.service;
+
+import org.kikomori.main.model.User;
+import org.kikomori.main.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(long id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(null);
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(long id, User user) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteById(long id) {
+        userRepository.deleteById(id);
+    }
+}
